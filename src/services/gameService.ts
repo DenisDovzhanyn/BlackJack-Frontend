d10737874ba39890fa6fa60bc2c705e58f5bb499
Denis <dovzhanyndenis03@gmail.com>
2025-05-20 16:11:21 -0400

    feat: implemented gameService methods which will be used to tell the backend what the user wants to do during the game

---
+++ src/services/gameService.ts
@@ -0,0 +1,100 @@
+
+
+export const hit = async (id: number) => {
+    try {
+        const resp = await fetch(`${import.meta.env.VITE_BASE_API}/game/hit`,
+            {
+                method: 'POST',
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({id})
+                
+            }
+        )
+        
+        const respJson = await resp.json()
+
+        if (resp.status != 200) {
+            throw new Error(respJson.error)
+        }
+
+        //* this will return a game object. i should probably define what that'll look like
+        return respJson
+    } catch (err) {
+        console.log(err)
+        if (err instanceof Error) return err
+    }
+}
+
+export const placeBet = async (id: number, betAmount: number, isInsuranceBet: boolean) => {
+    try {
+        const resp = await fetch(`${import.meta.env.VITE_BASE_API}/game/${isInsuranceBet ? 'insurance' : 'placebet'}`,
+            {
+                method: 'POST',
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({id, betAmount})
+                
+            }
+        )
+
+        const respJson = await resp.json()
+
+        if (resp.status != 200) {
+            throw new Error(respJson.error)
+        }
+
+        return respJson
+    } catch (err) {
+        console.log(err)
+        if (err instanceof Error) return err.message
+    }
+}
+
+export const stand = async (id: number) => {
+    try {
+        const resp = await fetch(`${import.meta.env.VITE_BASE_API}/game/stand`,
+            {
+                method: 'POST',
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({id})
+                
+            }
+        )
+
+        const respJson = await resp.json()
+
+        if (resp.status != 200) {
+            throw new Error(respJson.error)
+        }
+
+        return respJson
+    } catch (err) {
+        console.log(err)
+        if (err instanceof Error) return err.message
+    }
+}
+
+export const doubleDown = async (id: number) => {
+    try {
+        const resp = await fetch(`${import.meta.env.VITE_BASE_API}/game/doubledown`,
+            {
+                method: 'POST',
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({id})
+                
+            }
+        )
+
+        const respJson = await resp.json()
+
+        if (resp.status != 200) throw new Error(respJson.error)
+
+        return respJson
+    } catch (err) {
+        console.log(err)
+        if (err instanceof Error) return error.message
+    }
+}
